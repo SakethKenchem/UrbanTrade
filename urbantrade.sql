660405836255954a579b77498f4eb2c8e25ba89e
SakethKenchem <s.kenchem@gmail.com>
2023-12-12 18:47:48 +0300

    commit

--- urbantrade.sql
+++ urbantrade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2023 at 09:49 AM
+-- Generation Time: Dec 12, 2023 at 04:47 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,9 +33,17 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   `price` decimal(10,2) NOT NULL,
+  `category` varchar(200) DEFAULT NULL,
   `image_url` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `seller_id`, `name`, `description`, `price`, `category`, `image_url`, `created_at`) VALUES
+(1, 1, 'Horizon Forbidden West ps5 game', 'Horizon Forbidden West ps5 game', 5000.00, 'Video Games', 'productuploads/hfwps5.jpg', '2023-12-12 15:33:54');
 
 -- --------------------------------------------------------
 
@@ -53,6 +61,13 @@ CREATE TABLE `sellers` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `sellers`
+--
+
+INSERT INTO `sellers` (`seller_id`, `seller_name`, `email`, `phone_number`, `address`, `password`, `created_at`) VALUES
+(1, 'SakethTech', 's.kenchem@gmail.com', '0112716955', '07, Jubilee Apartments', '$2y$10$haWZPDGLH1wjyu2s/IfUueLtRvMwyM1.SFtw06K1Gb1iiUtBBR4XK', '2023-12-12 11:46:57');
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +83,13 @@ CREATE TABLE `users` (
   `location` varchar(200) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `phonenumber`, `location`, `created_at`) VALUES
+(1, 'Saketh Kenchem', 's.kenchem@gmail.com', '$2y$10$mifcAodDIjuSBaxOg10Vyu.FwkyEx0P1RYCHf7p.IC8gyEFU94uG.', '0112716955', '07, Jubilee Apartments, Mpaka Road', '2023-12-12 10:02:32');
 
 --
 -- Indexes for dumped tables
@@ -102,19 +124,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `sellers`
 --
 ALTER TABLE `sellers`
-  MODIFY `seller_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `seller_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
