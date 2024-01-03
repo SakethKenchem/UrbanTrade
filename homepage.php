7b8129390085009b868e79e42d475567246aa2bc
Retro_XI <74854083+powwwy@users.noreply.github.com>
2024-01-03 13:34:48 +0300

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -148,7 +148,6 @@ if (!empty($searchQuery)) {
         </style>
 </head>
 <body>
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="homepage.php" style="margin-top: 4px;">
